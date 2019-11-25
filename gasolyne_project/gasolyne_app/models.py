ab73bc06348766ae2d66369dda82973e90d7fa5e
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-25 10:19:47 -0500

    added three models

--- gasolyne_project/gasolyne_app/models.py
+++ gasolyne_project/gasolyne_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    username = models.CharField(max_length=100)
+    title = models.TextField()
+    description = models.TextField()
+    video_link = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.username
+
+
+class Like(models.Model):
+    isLike = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.isLike
