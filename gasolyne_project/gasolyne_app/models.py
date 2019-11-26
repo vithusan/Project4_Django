408b4872de8b039695099da1041c9b9119efe705
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-26 14:52:38 -0500

    create react client

--- gasolyne_project/gasolyne_app/models.py
+++ gasolyne_project/gasolyne_app/models.py
@@ -22,4 +22,4 @@ class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
 
     def __str__(self):
-        return f'${self.isLike}'
+        return f'{self.isLike}'
