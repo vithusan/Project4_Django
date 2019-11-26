0ff54ad8a72ab728ef454391100ac7405877456a
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-26 09:53:51 -0500

    imported models and serializers to create all three views

--- gasolyne_project/gasolyne_app/views.py
+++ gasolyne_project/gasolyne_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 
-# Create your views here.
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class LikeView(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
