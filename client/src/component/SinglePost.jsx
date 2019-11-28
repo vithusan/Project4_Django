6ec7c7f2127a72ba3dbffc352eedcd3bd491f396
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-11-27 19:51:53 -0500

    listed all the comments and likes in single post

--- client/src/component/SinglePost.jsx
+++ client/src/component/SinglePost.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class SinglePost extends Component {
+    state = {
+        username: '',
+        title: '',
+        description: '',
+        comments: [],
+        likes: []
+    }
+    componentDidMount = async () => {
+        const allComments = await axios.get(`/api/v1/post/${this.props.match.params.id}`)
+        this.setState(allComments.data)
+        console.log(allComments.data)
+    }
     render() {
         return (
             <div>
-                <h1>Single Page</h1>
+                {this.state.comments.map((comment) => {
+                    return (
+                        <div key={comment.id}>
+                            <h3>{comment.username}</h3>
+                            <p>{comment.content}</p>
+                        </div>
+                    )
+                })}
+                {this.state.likes.map((like) => {
+                    return (
+                        <div key={like.id}>
+                            <p>{like.id}</p>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
--- gasolyne_project/gasolyne_app/models.py
+++ gasolyne_project/gasolyne_app/models.py
@@ -1,25 +1,31 @@
 from django.db import models
 
+
 class Post(models.Model):
     username = models.CharField(max_length=150)
     title = models.CharField(max_length=255)
     description = models.TextField()
     # video_link = models.CharField(max_length=100)
+    # video_thumbnail = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     username = models.CharField(max_length=150)
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return self.content
 
+
 class Like(models.Model):
     isLike = models.BooleanField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes')
 
     def __str__(self):
         return f'{self.isLike}'
